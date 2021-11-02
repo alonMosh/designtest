4f82fc4e5dbcfdec96803e02caf030f4654807cd
alonMosh <alon151207@gmail.com>
2021-11-02 22:54:03 +0200

    made the about page.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ collapsibles.forEach((item) =>
         this.classList.toggle("collapsible--expanded");
     })
 );
+
+const switcher = document.querySelectorAll(".switcher__title");
+switcher.forEach((item) =>
+    item.addEventListener("click", function () {
+        this.classList.toggle("selected");
+    })
+);
+
+const faq = document.querySelectorAll(".q");
+faq.forEach((item) =>
+    item.addEventListener("click", function () {
+        this.classList.toggle("selected-question");
+    })
+);
+
